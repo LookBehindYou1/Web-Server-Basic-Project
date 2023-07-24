d0fcf20f8c3e2a8809b66a8722489f38ace9f552
LookBehindYou1 <chungdaniel910@gmail.com>
2023-07-24 07:56:55 -0400

    More changes.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { GameObject, AccelerationObject } from './Game/GameObject';
 
 function gaussianRandom(mean=0, stdev=1) {
   const u = 1 - Math.random(); // Converting [0,1) to (0,1]
@@ -15,37 +16,57 @@ function randomPosition(min, max) {
 
 function App() {
   // Variables are created.
-  const [x, setX] = React.useState(10);
-  const [y, setY] = React.useState(210);
-  const [xDirection, setXDirection] = React.useState(1);
-  const [yDirection, setYDirection] = React.useState(1);
+  const [trash, setTrash] = React.useState(newAccelerationObject(0, 0, Math.random()*750,
+                            Math.random()*750, 0, 0, '/Trash.png', 50, 50, 800, 800));
   const [stepSize, setStepSize] = React.useState(1);
   const [score, setScore] = React.useState(0);
+  const [time,setTime] = React.useState(0);
   const [imageSize,setImageSize] = React.useState(50);
-  // Refs for x and y are created.
-  const xRef = React.useRef(x);
-  const yRef = React.useRef(y);
-  const xDirectionRef = React.useRef(xDirection);
-  const yDirectionRef = React.useRef(yDirection);
-  // Updates variables when changed.
-  React.useEffect(() => {
-    xRef.current = x;
-    yRef.current = y;
-    xDirectionRef.current = xDirection;
-    yDirectionRef.current = yDirection;
-  }, [x, y, xDirection, yDirection]);
+  // Key Movement
+  const keyDown = (e) => {
+    switch (e.keyCode) {
+      // Left
+      case 37:
+        trash.dx = -1;
+        break
+      // Up
+      case 38:
+        trash.dy = -1;
+        break
+      // Right
+      case 39:
+        trash.dx = 1;
+        break
+      // Down
+      case 40:
+        trash.dy = 1;
+        break
+      // Random acceleration
+      case 32:
+        trash.ddy += gaussianRandom(0.01)
+        break
+      // Nothing happens
+      default:
+        console.log("Other key pressed: ", e.keyCode);
+        break
+    }
+    console.log(trash);
+  };
+
   // Every few milliseconds moves position.
   React.useEffect(() => {
     const id = setInterval(() => {
-      if (xRef.current <= 0) setXDirection(1);
-      if (xRef.current >= 600 - 50) setXDirection(-1);
-      if (yRef.current <= 0) setYDirection(1);
-      if (yRef.current >= 600 - 50) setYDirection(-1);
-      setX(prevX => prevX + xDirectionRef.current * stepSize + gaussianRandom() * stepSize);
-      setY(prevY => prevY + yDirectionRef.current * stepSize + gaussianRandom() * stepSize);
+      trash.step(stepSize)
+      setTime(oldTime => oldTime + stepSize/1000)
     }, 50);
     return () => clearInterval(id);
   }, [stepSize, imageSize]);
+
+  // IDK
+  React.useEffect(() => {
+    window.addEventListener('keydown', keyDown, false);
+    return () => window.removeEventListener('keydown', keyDown, false);
+  }, []);
 
   // What happens when you click.
   const determineScore = (container) => {
@@ -72,19 +93,9 @@ function App() {
   // Determines what the website looks like.
   return (
     <div className="Game-Container" onClick={() => determineScore(0)}>
-      <div className="game-score">{score}</div>
-      <img
-        alt= "Trash"
-        src="Trash.png"
-        style={{
-          position: 'absolute',
-          left: x,
-          top: y,
-          width: `${imageSize}px`,
-          height: `${imageSize}px`,
-        }}
-        onClick={() => determineScore(1)}
-      />
+      <div className="game-score">Time =
+      {time.toFixed(4)}, Score = {score}, Gravity = {trash.ddy.toFixed(8)}</div>
+        {trash.render({onClick: () => determineScore(1)})}
     </div>
   );
 }
